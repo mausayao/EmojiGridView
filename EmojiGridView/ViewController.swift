027af7251ce4ef4409fb642773dda77631d680cc
mauricio <mauricio.sayao@gmail.com>
2020-10-14 15:12:23 -0400

    Alpha

---
+++ EmojiGridView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiGridView
+//
+//  Created by Maurício de Freitas Sayão on 14/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
