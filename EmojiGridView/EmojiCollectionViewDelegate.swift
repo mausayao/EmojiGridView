90a28e5b73ac2ab8b31785885f48362baa4e0ccc
mauricio <mauricio.sayao@gmail.com>
2020-10-15 14:10:18 -0400

    Configurando layout do grid view

---
+++ EmojiGridView/EmojiCollectionViewDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  EmojiCollectionViewDelegate.swift
+//  EmojiGridView
+//
+//  Created by Maurício de Freitas Sayão on 15/10/20.
+//
+
+import UIKit
+
+
+class EmojiCollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout {
+    
+    let numberOfItemsPerRow: CGFloat
+    let interItemSpacing: CGFloat
+    
+    init(numberOfItemsPerRow: CGFloat, interItemSpacing: CGFloat) {
+        self.numberOfItemsPerRow = numberOfItemsPerRow
+        self.interItemSpacing = interItemSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxWidth = UIScreen.main.bounds.width
+        let totalSpacing =  interItemSpacing * numberOfItemsPerRow
+        
+        let itemWidth = (maxWidth - totalSpacing) / numberOfItemsPerRow
+        
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        interItemSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if section == 0 {
+            return UIEdgeInsets(top: 0, left: 0, bottom: interItemSpacing/2, right: 0)
+        } else {
+            return UIEdgeInsets(top: interItemSpacing / 2, left: 0, bottom: interItemSpacing / 2, right: 0)
+        }
+    }
+}
